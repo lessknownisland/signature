1733144cf4824a954686758f71b0f7b29ae3cadc
System Administrator <root@mac.local>
2020-01-22 15:31:33 +0800

    static error

---
+++ restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+config=config_dev.py
+function restart {
+port=$1
+pid=$(ps -ef |grep uwsgi |grep -v grep |grep ${port} |awk '{print $2}' |sort |uniq |head -1)
+echo "restarting port ${port}..."
+while [ ! -z ${pid} ]
+do
+    kill -9 ${pid}
+    echo "port ${port}: pid ${pid} killed..."
+    pid=$(ps -ef |grep uwsgi |grep -v grep |grep ${port} |awk '{print $2}' |sort |uniq |head -1)
+done
+nohup uwsgi --http 0.0.0.0:8088 --file signature/wsgi.py --processes 16 --threads 8 &
+sleep 3
+pid=$(ps -ef |grep uwsgi |grep -v grep |grep ${port} |awk '{print $2}' |sort |uniq |head -1)
+if [ ! -z ${pid} ];then
+    echo "port ${port} restarted ok."
+    ps -ef |grep uwsgi |grep -v grep |grep $port
+else
+    echo "port ${port} restarted failed. pls check!"
+    ps -ef |grep uwsgi |grep -v grep |grep $port
+fi
+}
+restart 8088
--- signature/urls.py
+++ signature/urls.py
@@ -18,6 +18,7 @@ from django.urls      import path
 from django.conf.urls import include, url
 from django.conf      import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # django管理后台
@@ -35,4 +36,9 @@ urlpatterns = [
     # 登陆控制及用户信息
     url(r'^', include('control.urls')),
     url(r'^control/', include('control.urls')),
+
+    # 配置静态文件的映射(在settings中我的静态文件配置使用的是STATICFILES_DIRS
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATICFILES_DIRS}, name='static'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
